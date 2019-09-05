0f095f39f679177b95b8a3427672e92137817c14
k26duran <k26duran@gmail.com>
2019-09-05 06:05:58 -0500

    avance

--- src/main/java/edu/escuelaing/arem/ASE/app/HttpService.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/HttpService.java
@@ -1,8 +1,15 @@
 package edu.escuelaing.arem.ASE.app;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -16,11 +23,15 @@ public class HttpService {
 	
 	Method[] listM;
 	HashMap<String, MethodHandler> listaURLHandler;
+	private static Socket clientSocket;
+	private static ServerSocket serverSocket = null;
+	String request ="";
+	private static final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 	
 	public void init() {
 		try {
 			//receive("WebServiceHello");
-			List<Class<?>> classes= new ArrayList<>();
+			List<Class<?>> classes= new ArrayList<Class<?>>();
 			classes= getClassesInPackage("app");
 			System.out.println(classes.toString());
 			for(Class<?> c:classes) {
@@ -35,15 +46,99 @@ public class HttpService {
 		
 	}
 	
-	public void listen() {
-	
+	public void listen() throws Exception{
+		try{
+			int port= getPort();
+			serverSocket = new ServerSocket(port);
+			System.out.println("Listen for port : " + port);
+		} catch(IOException e){
+			System.err.println("Could not listen on port: " + getPort());
+	        System.exit(1);
+		}
+		while(true){
+			try{
+				System.out.println("Ready to receive...");
+				clientSocket = serverSocket.accept();
+			} catch(IOException e){
+				System.err.println("Accept failed.");
+                System.exit(1);
+			}
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			String inputLine; 
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("Received: " + inputLine);
+				if (!in.ready()) {
+					break;
+				}
+				if(inputLine.contains("GET")){
+					request = inputLine.split(" ")[1];
+					System.out.println("Recurso solicitado:"+request);
+				}
+			}
+			if (request.contains("/apps")) {
+				returnApp(request);
+			} else if (request.contains("html")) {
+				System.out.println(request);
+				returnHtml(request);
+			} else if (request.contains("jpg")) {
+				returnImages(request, clientSocket);
+			} else{
+				notFound();
+			}
+			out.close();
+			in.close();
+
+		}
 
 	}
+	private static void notFound() throws IOException{
+		try{
+			PrintWriter out;
+			BufferedReader in = new BufferedReader(new FileReader(classLoader.getResource("notFound.html").getFile()));
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			String result = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n";
+			out.println(result);
+			while (in.ready()) {
+				out.println(in.readLine());
+			}
+			out.close();
+			in.close();
+
+		} catch(IOException e){
+			System.err.println("Error en notFound");
+		}
+	}
+
+	private static void returnApp(String request) throws IOException{
+
+	}
+	private static void returnHtml(String request) throws IOException{
+		try{
+			PrintWriter out;
+			BufferedReader in = new BufferedReader(new FileReader(classLoader.getResource(request).getFile()));
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			String result = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n";
+			out.println(result);
+			while (in.ready()) {
+				out.println(in.readLine());
+			}
+			out.close();
+			in.close();
+
+		} catch(IOException e){
+			System.err.println("Error en returnHtml");
+		}
+	}
+	private static void returnImages(String request, Socket clientS) throws IOException{
+
+	}
+	
 	public void receive(String direccion){
 		try {
 			Class<?> c= Class.forName(direccion);
 			listM= c.getDeclaredMethods();
-			listaURLHandler = new HashMap<>();
+			listaURLHandler = new HashMap<String, MethodHandler>();
 			
 			for(Method e: listM) {
 				if(e.isAnnotationPresent(Web.class)) {
@@ -61,7 +156,7 @@ public class HttpService {
 	}
 	public static final List<Class<?>> getClassesInPackage(String packageName) {
 	    String path = packageName.replaceAll("\\.", File.separator);
-	    List<Class<?>> classes = new ArrayList<>();
+	    List<Class<?>> classes = new ArrayList<Class<?>>();
 	    String[] classPathEntries = System.getProperty("java.class.path").split(
 	            System.getProperty("path.separator")
 	    );
@@ -104,4 +199,11 @@ public class HttpService {
 
 	    return classes;
 	}
+
+	public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
